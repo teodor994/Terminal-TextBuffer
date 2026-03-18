010691fb1f5e0eed8b6803d6994cee4933d9312d
Teodor Andrei Vica <teodorvica@gmail.com>
2026-03-18 17:02:29 +0200

    Added "drawToSystemConsole" function which outputs the terminal to the screen

--- src/main/java/com/analysis/TerminalBuffer.java
+++ src/main/java/com/analysis/TerminalBuffer.java
@@ -1,6 +1,5 @@
 package com.analysis;
 import java.util.Arrays;
-//import java.stream.
 
 public class TerminalBuffer {
     // the buffer
@@ -9,11 +8,11 @@ public class TerminalBuffer {
     // shell configuration
     private int width;
     private int height;
-    private int total_capacity;
-    private int max_scrollback;
+    private int totalCapacity;
+    private int maxScrollback;
 
-    private int start_ptr = 0;
-    private int total_lines = 0;
+    private int startPtr = 0;
+    private int totalLines = 0;
 
     private int cursorCol = 0;
     private int cursorRow = 0;
@@ -21,4 +20,37 @@ public class TerminalBuffer {
     private int currentFg = 0; // 0 = Default
     private int currentBg = 0;
     private boolean bold, italic, underline;
+
+    public TerminalBuffer(int width, int height, int maxScrollback) {
+        this.width = width;
+        this.height = height;
+        this.maxScrollback = maxScrollback;
+        this.totalCapacity = height + maxScrollback;
+        this.buffer = new Cell[totalCapacity][width];
+
+        for (int r = 0; r < totalCapacity; r++) {
+            for (int c = 0; c < width; c++) {
+                buffer[r][c] = new Cell();
+            }
+        }
+
+        this.totalLines = height;
+    }
+
+    public void drawToSystemConsole() {
+        System.out.print("\033[H");
+
+        for (int r = 0; r < height; r++) {
+            for (int c = 0; c < width; c++) {
+                Cell cell = getCellAtScreenPos(c, r);
+
+                if (cell.bold) System.out.print("\033[1m");
+
+                System.out.print(cell.character);
+
+                System.out.print("\033[0m");
+            }
+            System.out.println();
+        }
+    }
 }
